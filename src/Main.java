68fac945f302943f2f4cfe05ed1dbe78bc1d158b
SuperNickmann <niklas-falk001@gmx.de>
2025-11-01 22:59:05 +0100

    Weitere Testfälle

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        MeineKlasse obj = new MeineKlasse();
-        int result = obj.add(2, 3);
+        MeineKlasse obj1 = new MeineKlasse();
+        MeineKlasse obj2 = new MeineKlasse();
+        int result = obj1.add(2, 3);
+        int result2 = obj2.add(4, 5);
         System.out.println("Ergebnis von 2 + 3 = " + result);
+        System.out.println("Ergebnis von 4 + 5 = " + result2);
 
         if(result == 5) {
+            System.out.println("Test erfolgreich!");
+        } else {
+            System.out.println("Test fehlgeschlagen!");
+            System.exit(1); // CI schlägt fehl
+        }
+
+        if(result2 == 8){
             System.out.println("Test erfolgreich!");
         } else {
             System.out.println("Test fehlgeschlagen!");
